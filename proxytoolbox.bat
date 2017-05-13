c821836f9798e5db3a036106e62dd9d0e89bf7d6
theo546 <theo546@live.fr>
2017-05-13 02:00:50 +0200

    Add files via upload

---
+++ proxytoolbox.bat
@@ -0,0 +1,60 @@
+@echo off
+:hello
+cls
+echo whellcome to proxy toolbox!!!1!
+echo select the option:
+echo.
+echo 1 anvitirus
+echo 2 matrix
+echo 3 pornhub free premium account
+echo 4 freddo mercure leaked song
+echo 5 internet explorer CHOOSED AS BEST BROWSER FOR 2000001 YEAR!!!!1
+set /p VarQuestion= 
+if %VarQuestion%==1 goto antivirus
+if %VarQuestion%==2 goto matrix
+if %VarQuestion%==3 goto pornhub
+if %VarQuestion%==4 goto freddomercure
+if %VarQuestion%==5 goto iexplore
+goto hello
+
+:antivirus
+echo hello scaning for virus:
+echo %RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%
+ping 127.0.0.1 -n 2 > nul
+color fc
+echo virus fucking found: C:\Windows\system32
+ping 127.0.0.1 -n 1 > nul
+echo deleting it automaticly
+ping 127.0.0.1 -n 5 > nul
+goto matrix
+
+:matrix
+echo %RANDOM%%RANDOM%%RANDOM%%RANDOM%%RANDOM%
+color fc
+goto matrix
+
+:pornhub
+ping 127.0.0.1 -n 2 > nul
+echo hello pornhub are you sure you wanna spam this (it will spam pornhub be sure before start this shit XD)
+pause
+goto pornhub2
+
+:pornhub2
+iexplore http://pornhub.com/
+iexplore https://fr.pornhub.com/view_video.php?viewkey=ph58dffd8d211b4
+iexplore https://fr.pornhub.com/view_video.php?viewkey=1762980755
+goto pornhub2
+
+:freddomercure
+echo starting freddo mercure with best browser internet explorer
+ping 127.0.0.1 -n 3 > nul
+iexplore https://www.youtube.com/watch?v=d_MwWQEAOEI
+goto hello
+
+:iexplore
+echo thx for choosing iexplore!!1
+ping 127.0.0.1 -n 3 > nul
+iexplore
+goto hello
+
+#COPYFUCK_PROXY_2017_GPLv3
