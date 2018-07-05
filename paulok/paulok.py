37dc7e05413f3ca978624f0c10952b1ed04caa84
theo546 <theo546@live.fr>
2018-07-05 23:43:33 +0000

    Update magrosseteub.py

--- paulok/paulok.py
+++ paulok/paulok.py
@@ -15,11 +15,14 @@ bien cordialement, la direction
 
 """
 
+# On importe les libraries.
 import tweepy, random
 
+# On met les truc hyper secret ici.
 consumer_token, consumer_secret = "", ""
 access_key, access_secret = "", ""
 
+# Ici, le script va tenter de se connecter a Twitter, si il n'y arrive pas il se coupe.
 try:
 	auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 	auth.set_access_token(access_key, access_secret)
@@ -29,12 +32,14 @@ try:
 	tweets = [[tweet.full_text] for tweet in stuff]
 except: quit()
 
+# La loop ici va checher un tweet random, regarde son contenue et regarde si il y a un lien d'image, une mention ou un lien YouTube, si oui, la loop se relance afin de trouver ze tweet parfait.
 while True:
 	superrandomdetesmort = random.randint(0, 200)
 	text = str(tweets[superrandomdetesmort])
 	if "t.co" in text or "@" in text or "youtu.be" in text: continue
 	else: break
 
+# Ici c'est surement la partie du script la plus dégeulasse, ça va enlever les [ ] \ ' du string sinon c'est deg.
 text = text.replace('\\n', '\n')
 text = text.replace('["', '', 1)
 text = text.replace('"]', '', 1)
@@ -42,4 +47,5 @@ text = text.replace("['", '', 1)
 text = text.replace("']", '', 1)
 text = text.replace("\\", '', 1)
 
+# Ça vous imprime le message sur l'imprimante la plus proche comme print ça veux dire imprimer mdr
 print(text)
